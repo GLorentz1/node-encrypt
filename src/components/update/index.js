a3d07c408df926e64a3065d162aa9b22e7703ef9
Gustavo Lorentz <gus.aca.lo@gmail.com>
2025-03-27 22:24:14 -0300

    Creating update file endpoint

---
+++ src/components/update/index.js
@@ -0,0 +1,19 @@
+const { upload }  = require("./update")
+
+const S3FileRepository = require("../../infrastructure/S3FileRepository");
+const DynamoFileRecordRepository = require("../../infrastructure/DynamoFileRecordRepository");
+const EncryptionService = require("../../infrastructure/EncryptionService");
+const {S3Client} = require("@aws-sdk/client-s3");
+const AWS = require("aws-sdk");
+
+const s3Client = new S3Client();
+const BUCKET_NAME = process.env.S3_BUCKET;
+const s3Repository = new S3FileRepository(s3Client, BUCKET_NAME);
+
+const dynamoClient =  new AWS.DynamoDB.DocumentClient();
+const dynamoRepository = new DynamoFileRecordRepository(dynamoClient);
+
+const encryptionService = new EncryptionService();
+const { handler } =  upload(s3Repository, dynamoRepository, encryptionService);
+
+module.exports.handler = handler;
---
+++ src/components/update/update.js
@@ -0,0 +1,47 @@
+
+const TABLE_NAME = process.env.DYNAMODB_TABLE;
+
+module.exports.update = (fileRepository, fileRecordRepository, encryptionService) => {
+    const handler = async (event) => {
+        try {
+            const {uuid, filename, password} = JSON.parse(event.body);
+
+            let record = await fileRecordRepository.get(TABLE_NAME, {id: uuid});
+
+            const isMatch = await encryptionService.compare(password, record.hashedPassword)
+            if (!isMatch) {
+                return {statusCode: 400, body: JSON.stringify({reason: "Passwords don't match."})};
+            }
+
+            if (record.status !== "encrypted") {
+                return {statusCode: 403, body: JSON.stringify({reason: "File can't be updated."})};
+            }
+
+            await fileRecordRepository.update(TABLE_NAME, uuid, "filename", filename);
+            await fileRecordRepository.update(TABLE_NAME, uuid, "status", "pending_upload");
+
+            const uploadUrl = await fileRepository.generateUploadUrl({key: `uploads/${uuid}/${filename}`})
+            await fileRepository.delete({key: `encrypted/${uuid}/${record.filename}`})
+
+            return {
+                statusCode: 200,
+                contentType: "application/json",
+                body: JSON.stringify({
+                    id: uuid,
+                    uploadUrl: uploadUrl,
+                }),
+            };
+
+        } catch (error) {
+            return {
+                statusCode: 500,
+                contentType: "application/json",
+                body: JSON.stringify(
+                    {message: "Upload failed.", error: error.message}
+                )
+            }
+        }
+    }
+
+    return {handler};
+};
